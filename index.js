988d134812f51475245a52394001367b94169d7e
Junaedi Akbar <juned.akb@gmail.com>
2024-04-16 17:48:30 +0800

    add cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ process.env.TZ = 'Asia/Makassar';
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
